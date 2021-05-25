850a1bef792fde53a34446ddd894590edf83beeb
xingzhe <19021@qq.com>
2021-05-25 22:15:11 +0800

    第二版

--- src/main/java/com/xz/controller/HomeController.java
+++ src/main/java/com/xz/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.xz.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,12 @@ import java.util.Date;
  */
 @Controller
 public class HomeController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/home")
     @ResponseBody
     public String home() {
+        log.info("ok");
         return "hi, baby!!!";
     }
 
